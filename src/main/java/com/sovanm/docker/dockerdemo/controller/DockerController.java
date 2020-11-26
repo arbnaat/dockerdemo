bf943cff972eb4785fcaf919cd4413ce0676c068
unknown <arbnaat@gmail.com>
2020-11-26 21:43:57 +0530

    wroking docker

--- src/main/java/com/sovanm/docker/dockerdemo/controller/DockerController.java
+++ src/main/java/com/sovanm/docker/dockerdemo/controller/DockerController.java
@@ -9,6 +9,6 @@ public class DockerController {
 	@GetMapping("/name")
 	public String getMyName() {
 		
-		return "Simple Spring Boot Application";
+		return "Simple Spring Boot docker Application";
 	}
 }
